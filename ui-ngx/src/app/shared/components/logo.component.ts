30c6ef46e836919cf15272e240b4d755a4c55b5b
Truong Pham Xuan <xuantruonglk02@gmail.com>
2026-08-10 22:11:21 +0700

    [tyne/feature/tyne] Update logo and favicon to use new Tyne branding

--- ui-ngx/src/app/shared/components/logo.component.ts
+++ ui-ngx/src/app/shared/components/logo.component.ts
@@ -23,7 +23,7 @@ import { Component } from '@angular/core';
 })
 export class LogoComponent {
 
-  logo = 'assets/logo_title_white.svg';
+  logo = 'assets/logo_title_white.png';
 
   gotoThingsboard(): void {
     window.open('https://thingsboard.io', '_blank');
